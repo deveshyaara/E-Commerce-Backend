13c35fb625add4a142940168b0f1f6928999b290
deveshyaara <tiwaridevesh887@gmail.com>
2024-05-07 23:27:25 +0530

    Middleware has been added 	new file:   middlewares/auth_mw.js 	modified:   routes/auth.routes.js

---
+++ middlewares/auth_mw.js
@@ -0,0 +1,42 @@
+// it will check that body is perfect and correct
+const verifySignUpBody = async (req, res, next)=>{
+    try{
+         //Check for the name
+         if(!req.body.name){
+            return res.status(400).send({
+                message : "Failed ! Name was not provied in request body"
+            })
+        }
+         //check for the email
+         if(!req.body.email){
+            return res.status(400).send({
+                message : "Failed ! email was not provied in request body"
+            })
+        }
+
+         //check for the userId
+         if(!req.body.userId){
+            return res.status(400).send({
+                message : "Failed ! userId was not provied in request body"
+            })
+        }
+          //Check if the user with the same userId is already present
+          const user = await user_model.findOne({userId : req.body.userId})
+
+          if(user){
+              return res.status(400).send({
+                  message : "Failed ! user with same userId is already present"
+              })
+          }
+          next()
+
+    }catch(err){
+        console.log("Error while validating request body")
+        res.status(500),send({
+            message : "Error while validating request body"
+        })
+    }
+}
+module.exports = {
+    verifySignUpBody : verifySignUpBody
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@
 //  Intercepting this
 
 const authController = require("../controllers/auth.controller")
+const authMW = require("../middlewares/auth_mw")
 module.exports = (app)=>{
-    app.post("/ecomm/api/v1/auth/signup", authController.signup)
+    app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
 }
