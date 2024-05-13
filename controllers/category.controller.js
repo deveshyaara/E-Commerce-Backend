bd7427c916368400cbe5d93c6260220d6bb9ca58
deveshyaara <[tiwaridevesh887@gmail.com]>
2024-05-13 22:21:52 +0530

    Get: Find category based on name Get: Find all categories Get: Find category by ID Put: Edit a category Del: Delete category by ID     modified:   controllers/category.controller.js 	modified:   routes/category.routes.js

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -20,3 +20,69 @@ exports.createNewCategory = async (req, res)=>{
    
     // return the response of the created category
 }
+exports.findCategoryByName = async (req, res) => {
+    const name = req.params.name;
+    try {
+        const category = await category_model.findOne({ name })
+        if (!category) {
+            return res.status(404).json({ message: "Category not found" })
+        }
+        return res.status(200).send(category)
+    } catch (err) {
+        console.error("Error while finding category:", err);
+        return res.status(500).send({ message: "Internal server error" })
+    }
+}
+// Controller function to retrieve all categories
+exports.getAllCategories = async (req, res) => {
+    try {
+        const categories = await category_model.find()
+        res.status(200).send(categories);
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+// Controller function to retrieve a category by ID
+exports.getCategoryById = async (req, res) => {
+    const categoryId = req.params.categoryId
+    try {
+        const category = await category_model.findById(categoryId)
+        if (!category) {
+            return res.status(404).send({ message: "Category not found" })
+        }
+        res.status(200).send(category)
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+// Controller function to update a category by ID
+exports.updateCategoryById = async (req, res) => {
+    try {
+        const categoryId = req.params.categoryId;
+        const { name, description } = req.body;
+        
+        const updatedCategory = await category_model.findByIdAndUpdate(categoryId, { name, description }, { new: true });
+        if (!updatedCategory) {
+            return res.status(404).send({ message: "Category not found" });
+        }
+        
+        res.status(200).send(updatedCategory);
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+// Controller function to delete a category by ID
+exports.deleteCategoryById = async (req, res) => {
+    try {
+        const categoryId = req.params.categoryId;
+        const deletedCategory = await category_model.findByIdAndDelete(categoryId);
+        if (!deletedCategory) {
+            return res.status(404).send({ message: "Category not found" });
+        }
+        res.status(200).send({ message: "Category deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -2,5 +2,18 @@
 const catController = require("../controllers/category.controller")
 const auth_mw = require("../middlewares/auth_mw")
 module.exports = (app)=>{
+    // Route for creating a new category
     app.post("/ecomm/api/v1/categories",[auth_mw.verifyToken], catController.createNewCategory)
+    // Route for finding a category based on its name
+    app.get("/ecomm/api/v1/categories/:name", catController.findCategoryByName)
+    // Retrieve all categories
+    app.get("/ecomm/api/v1/categories", catController.getAllCategories)
+    // Retrieve a category by ID
+    app.get("/ecomm/api/v1/categories/:categoryId", catController.getCategoryById)
+
+    // Update a category by ID
+    app.put("/ecomm/api/v1/categories/:categoryId", catController.updateCategoryById)
+
+    // Delete a category by ID
+    app.delete("/ecomm/api/v1/categories/:categoryId", catController.deleteCategoryById)
 }
